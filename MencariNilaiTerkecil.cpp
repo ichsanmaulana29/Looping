a9ff3d5afe8d7a19a7397b2431dc3d53ab0414da
Ichsan Maulana <37248817+ichsanmaulana29@users.noreply.github.com>
2018-09-18 11:40:36 +0700

    Create MencariNilaiTerkecil.cpp

---
+++ MencariNilaiTerkecil.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+
+/*
+	Program  	  : Terkecil dari 2 bilangan
+	Deskripsi 	: mencari nilai terkecil
+	Penulis   	: Ichsan Maulana
+	Tanggal   	: 09 Juni 2017
+*/
+
+
+main()
+{
+	//Program utama
+	//deklarasi variabel
+	int a,b;
+	//input data
+	printf("===Program Pencari Nilai Terkecil=== \n\n");
+	printf("Masukan Nilai Pertama = ");
+	scanf("%d",&a);
+	printf("Masukan Nilai Kedua = ");
+	scanf("%d",&b);
+	//proses pemilihan
+	if(a<b){
+		printf("Bilangan terkecil adalah %d",a);
+		}
+			else if (b<a) {
+			printf("Bilangan terkecil adalah %d",b);
+			}
+				else{
+				printf("Nilai Tidak Ada Yang Besar Dan Kecil");
+				}
+	}
